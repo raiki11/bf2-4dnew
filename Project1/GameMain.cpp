af33178b70cf6f4bdb9347a82d76085ff1cbc83e
ryu943 <120348162+ryu943@users.noreply.github.com>
2023-06-30 15:26:48 +0900

    pose

--- Project1/GameMain.cpp
+++ Project1/GameMain.cpp
@@ -1,6 +1,6 @@
 #include "GameMain.h"
 #include"DxLib.h"
-
+#include"PadInput.h"
 
 GameMain::GameMain()
 {
@@ -12,10 +12,30 @@ GameMain::~GameMain()
 
 AbstractScene* GameMain::Update()
 {
+	//ポーズフラグ切り替え処理
+	if (PAD_INPUT::OnButton(XINPUT_BUTTON_START))
+	{
+		PauseFlg = !PauseFlg;
+
+	}
+	//ポーズ中でないとき
+	if (PauseFlg = FALSE) {
+		//main処理
+	}
+
+	// ポーズ中
+	if (PauseFlg = TRUE) {
+		
+	}
 	return this;
 }
 
 void GameMain::Draw() const
 {
 	DrawFormatString(0, 0, 0xffffff, "ゲームメイン");
+	//一時停止中の描画
+	if (PauseFlg == TRUE)
+	{
+		
+	}
 }
