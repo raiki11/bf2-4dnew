13cc095f49243a36983f23de4b7100a407232a2a
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2023-07-04 14:56:45 +0900

    サカナ追加２

---
+++ Project1/Fish.cpp
@@ -0,0 +1,29 @@
+#include<DxLib.h>
+#include<time.h>
+#include "fish.h"
+Fish::Fish()
+{
+	FishImg = LoadGraph("../images/Enemy/Enemy_FishAnimation.png");
+}
+
+Fish::~Fish()
+{
+
+}
+
+AbstractScene* Fish::Update()
+{
+	// —”‚Ì¶¬
+	srand((unsigned int)time(NULL));
+
+	sakana = rand() % 100;
+
+	return 0;
+}
+
+void Fish::Draw() const
+{
+	if (sakana <= 30) {
+		DrawGraph(320, 450, FishImg, TRUE);
+	}
+}
--- Project1/Fish.h
+++ Project1/Fish.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "AbstractScene.h"
+
+class Fish : public AbstractScene {
+private:
+
+public:
+
+	// コンストラクタ
+	Fish();
+
+	// デストラクタ
+	~Fish();
+
+	AbstractScene* Update() override;
+
+	void Draw() const override;
+
+	int sakana;
+	int FishImg;
+};
