8b0e06217ad31d057477a5a400967f7509c649c2
ryu943 <120348162+ryu943@users.noreply.github.com>
2023-07-12 12:15:40 +0900

    分割読込

--- Project1/UI.cpp
+++ Project1/UI.cpp
@@ -2,6 +2,7 @@
 #include "DxLib.h"
 UI::UI()
 {
+
 	score= LoadGraph("images/UI/UI_Score.png");
 	top= LoadGraph("images/UI/UI_HiScore.png");
 	phase= LoadGraph("images/UI/UI_Phase.png");
@@ -9,6 +10,9 @@ UI::UI()
 	p_score = 0;
 	p_top = 0;
 	p_phase = 0;
+	i = 0;
+	Flag = FALSE;
+	LoadDivGraph("images/UI/UI_NumAnimation.png", 10, 1, 10, 32, 32, Num, TRUE); // âÊëúÇÃï™äÑì«Ç›çûÇ›
 }
 
 UI::~UI()
@@ -17,6 +21,7 @@ UI::~UI()
 
 void UI::DrawUI() const
 {
+	DrawRotaGraph(0,0,1.0,0,Num[1],TRUE,Flag);
 	DrawGraph(50,30 , score, TRUE);
 	DrawGraph(220, 30, top, TRUE);
 	DrawGraph(220, 50, phase, TRUE);
