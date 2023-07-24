81ae58fc50d4b1d847d1c0db89691c92a3522534
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-07-24 14:57:43 +0900

    player img update

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -35,7 +35,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	try // シーンを選ぶとそこからスタートする（最終的にはTitleを入れる）
 	{
-		sceneMng = new SceneManager((AbstractScene*)new GameMain());
+		sceneMng = new SceneManager((AbstractScene*)new TitleScene());
 
 	}
 	catch (const char* err)
