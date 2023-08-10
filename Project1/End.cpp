ccf6a8f74865a61e15fa706deeaa3350603c971a
ryu943 <120348162+ryu943@users.noreply.github.com>
2023-08-10 11:31:13 +0900

    BGM

--- Project1/End.cpp
+++ Project1/End.cpp
@@ -10,14 +10,21 @@ End::End()
 	for (int i = 0; i <= Stage::EnemyMax[Stage::Snum]; i++) {
 		enemy[i] = new Enemy(i, i);
 	}
+	//SE‚Ì“Ç‚İ‚İ
+	(SE = LoadSoundMem("sounds/SE_GameOver.wav"));
 }
 
 End::~End()
 {
+	//SE‚Ìíœ
+	DeleteSoundMem(SE);
 }
 
 AbstractScene* End::Update()
 {
+	
+	PlaySoundMem(SE, DX_PLAYTYPE_BACK, TRUE);
+	
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A)) {
 		return new TitleScene();
 	}
